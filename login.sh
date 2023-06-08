835fc284be1215d2f425d0acef97d1617e7af4dd
shakram02 <ahmedhamdyau@gmail.com>
2023-06-08 14:20:28 +0300

    update example env and login script

--- login.sh
+++ login.sh
@@ -1,2 +1,5 @@
-# login.sh
-aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin [ACCOUNT_NUMBER].dkr.ecr.ap-southeast-1.amazonaws.com
+#!/bin/bash
+
+set -ex
+source .env
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin ${DOCKER_REGISTERY_ACCOUNT}
--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
 
-aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin [account_id].dkr.ecr.ap-southeast-1.amazonaws.com \
+set -ex
+source .env
+source ./login.sh \
 && docker image prune -f && docker compose down && docker compose pull && docker compose up -d && docker compose logs -f
